18e6676e306034c9c4cd62a6313414c31256024b
Eduardo <ewaldo.jefe@gmail.com>
2025-07-06 20:14:30 -0400

    api base update

--- src/main/java/com/icc490/bike/desktop/model/RecordRequest.java
+++ src/main/java/com/icc490/bike/desktop/model/RecordRequest.java
@@ -4,40 +4,61 @@ public class RecordRequest {
     private String studentId;
     private String studentName;
     private String bicycleDescription;
+    private Long rackId; // Nuevo campo
+    private Long hook;   // Nuevo campo
 
-    public RecordRequest(String studentId, String studentName, String bicycleDescription) {
+    // Constructor completo
+    public RecordRequest(String studentId, String studentName, String bicycleDescription, Long rackId, Long hook) {
         this.studentId = studentId;
         this.studentName = studentName;
         this.bicycleDescription = bicycleDescription;
+        this.rackId = rackId;
+        this.hook = hook;
     }
 
     public RecordRequest() {
     }
 
-    //Getters y Setters
-
+    // Getters
     public String getStudentId() {
         return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
     }
 
     public String getStudentName() {
         return studentName;
     }
 
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
     public String getBicycleDescription() {
         return bicycleDescription;
     }
 
+    public Long getRackId() {
+        return rackId;
+    }
+
+    public Long getHook() {
+        return hook;
+    }
+
+    // Setters
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
     public void setBicycleDescription(String bicycleDescription) {
         this.bicycleDescription = bicycleDescription;
+    }
+
+    public void setRackId(Long rackId) {
+        this.rackId = rackId;
+    }
+
+    public void setHook(Long hook) {
+        this.hook = hook;
     }
 
     @Override
@@ -46,6 +67,8 @@ public class RecordRequest {
                 "studentId='" + studentId + '\'' +
                 ", studentName='" + studentName + '\'' +
                 ", bicycleDescription='" + bicycleDescription + '\'' +
+                ", rackId=" + rackId +
+                ", hook=" + hook +
                 '}';
     }
 }
